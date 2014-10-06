1749ec9623fd986c888cba81b0bc01572bbc730c
george <rpubaddr0@gmail.com>
2014-10-06 12:33:11 -0600

    Added metadata files.

---
+++ README.rst
@@ -0,0 +1,12 @@
+Chaser
+======
+
+Community package management for Chakra.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Chaser is a simple command-line tool and Python library for managing
+packages from the `[Chakra Community Repository]`_. It is written from
+scratch in idiomatic Python 3.
+
+
+.. _Chakra Community Repository: http://chakraos.org/ccr
---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+from chaser import __version__
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='chaser',
+    version=__version__,
+    packages=['chaser'],
+    include_package_data=True,
+    install_requires=[],
+    license='BSD',
+    description='Community package management for Chakra.',
+    long_description=README,
+    url='https://github.com/rshipp/chaser',
+    author='Ryan Shipp',
+    author_email='python@rshipp.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Software Distribution',
+    ],
+)
