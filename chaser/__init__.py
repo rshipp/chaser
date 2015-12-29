57c34cae582764b69bb32faa712110a46df69dde
rshipp <github@rshipp.com>
2015-12-30 00:16:44 +0100

    Add initial function for get_source_files

--- chaser/__init__.py
+++ chaser/__init__.py
@@ -1,1 +1,13 @@
 __version__ = "0.1"
+
+import requests
+import io
+import tarfile
+
+import ccr
+
+def get_source_files(pkgname, workingdir):
+    """Download the source tarball and extract it"""
+    r = requests.get(ccr.getpkgurl(pkgname))
+    tar = tarfile.open(mode='r', fileobj=io.BytesIO(r.content))
+    tar.extractall(workingdir)
