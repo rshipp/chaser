91cd50825643bd6b28b9d5321544a207de613b69
rshipp <github@rshipp.com>
2015-12-30 20:29:14 +0100

    Add argparser

--- chaser/__init__.py
+++ chaser/__init__.py
@@ -1,4 +1,16 @@
 __version__ = "0.1"
 
+import argparse
+
+from chaser import chaser
+
 def main():
-    print("Chaser")
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    parser_i = subparsers.add_parser('install')
+    parser_i.add_argument('package')
+    parser_i.set_defaults(func=chaser.install)
+
+    args = parser.parse_args()
+    args.func(args.package)
