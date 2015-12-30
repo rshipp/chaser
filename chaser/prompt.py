18fd2fd71abe552b8033e83246077b4ae246a301
rshipp <github@rshipp.com>
2015-12-30 21:34:46 +0100

    Write tests for prompt

--- chaser/prompt.py
+++ chaser/prompt.py
@@ -1,7 +1,12 @@
 YES = "y"
 NO = "n"
 
+def user_input(message):
+    """Substitutable input prompt"""
+    return input(message)
+
 def prompt(message, default=YES):
+    """Handle pacman-like prompting overhead"""
     if default == YES:
         yes = YES.upper()
         no = NO.lower()
@@ -11,7 +16,7 @@ def prompt(message, default=YES):
         no = NO.upper()
         check = YES
 
-    response = input("{message} [{y}/{n}] ".format(message=message, y=yes, n=no))
+    response = user_input("{message} [{y}/{n}] ".format(message=message, y=yes, n=no))
 
     if response.lower() == check.lower():
         return check
---
+++ tests/test_prompt.py
@@ -0,0 +1,40 @@
+import unittest
+from chaser import prompt
+
+
+class TestPrompt(unittest.TestCase):
+
+    def test_yes_yes_case(self):
+        prompt.user_input = lambda x: prompt.YES
+        r = prompt.prompt("test")
+        self.assertEquals(prompt.YES, r)
+
+    def test_yes_no_case(self):
+        prompt.user_input = lambda x: prompt.NO
+        r = prompt.prompt("test")
+        self.assertEquals(prompt.NO, r)
+
+    def test_no_yes_case(self):
+        prompt.user_input = lambda x: prompt.YES
+        r = prompt.prompt("test", prompt.NO)
+        self.assertEquals(prompt.YES, r)
+
+    def test_no_no_case(self):
+        prompt.user_input = lambda x: prompt.NO
+        r = prompt.prompt("test", prompt.NO)
+        self.assertEquals(prompt.NO, r)
+
+    def test_correct_lower_upper(self):
+        def user_input1(x):
+            self.assertEquals(prompt.YES.upper(), x[2])
+            self.assertEquals(prompt.NO.lower(), x[4])
+            return prompt.YES
+        prompt.user_input = user_input1
+        r = prompt.prompt("")
+
+        def user_input2(x):
+            self.assertEquals(prompt.YES.lower(), x[2])
+            self.assertEquals(prompt.NO.upper(), x[4])
+            return prompt.YES
+        prompt.user_input = user_input2
+        r = prompt.prompt("", prompt.NO)
