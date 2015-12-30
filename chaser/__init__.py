0a564ab4077b18113de626edf8438b377c4283c4
rshipp <github@rshipp.com>
2015-12-30 15:57:42 +0100

    Fix dependency resolution, add pacman module

--- chaser/__init__.py
+++ chaser/__init__.py
@@ -4,10 +4,13 @@ import io
 import tarfile
 import subprocess
 import json
+import re
 
 import requests
 from toposort import toposort_flatten
 import ccr
+
+from chaser import pacman
 
 def get_source_files(pkgname, workingdir):
     """Download the source tarball and extract it"""
@@ -15,18 +18,39 @@ def get_source_files(pkgname, workingdir):
     tar = tarfile.open(mode='r', fileobj=io.BytesIO(r.content))
     tar.extractall(workingdir)
 
-def _depends(pkgname):
+def recurse_depends(pkgname, graph=None):
+    if graph is None:
+        graph = {}
+
+    if graph.get(pkgname) is not None:
+        # End case: already traversed
+        return graph
+    elif pacman.exists(pkgname):
+        # End case: exists in pacman
+        graph[pkgname] = set()
+        return graph
+
+    # Otherwise get dependencies
+    graph[pkgname] = set()
     get_source_files(pkgname, ".")
     output = subprocess.check_output(["pkgvars.sh",
         "./{pkgname}/PKGBUILD".format(pkgname=pkgname)])
     data = json.loads(output.decode())['variables']
-    return data.get('makedepends', []) + data.get('depends', [])
+    # NOTE: We don't differentiate make/depends here, this is an area for
+    # improvement in the future if someone cares.
+    depends = data.get('makedepends', []) + data.get('depends', [])
+    # Only depends that are not already installed
+    for dep in depends:
+        depname = re.split('[>=<]', dep)[0]
+        if not pacman.is_installed(depname):
+            graph[pkgname].add(depname)
+
+    for dep in graph[pkgname]:
+        recurse_depends(dep, graph)
+
+    return graph
 
 def dependency_chain(pkgname):
     """Return an ordered list of dependencies for a package"""
-    depends = {
-        pkgname: _depends(pkgname),
-    }
-    for dep in depends[pkgname]:
-        depends[dep] = _depends(dep)
+    depends = recurse_depends(pkgname)
     return toposort_flatten(depends)
---
+++ chaser/pacman.py
@@ -0,0 +1,16 @@
+import subprocess
+
+def is_installed(pkgname):
+    realname = exists(pkgname)
+    if realname:
+        return not subprocess.call(['pacman', '-Qi', realname],
+                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    return False
+
+def exists(pkgname):
+    try:
+        return subprocess.check_output(['pacman', '-Sp', '--print-format',
+            '%n', pkgname], stderr=subprocess.DEVNULL).split().pop()
+                
+    except subprocess.CalledProcessError:
+        return False
