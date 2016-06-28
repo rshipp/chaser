9c5011228ea46f975e1ade8da18e720fd93522fd
rshipp <github@rshipp.com>
2016-06-28 17:01:14 -0600

    Import and install translation files

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import os
+import glob
+import shutil
 from setuptools import setup
+from setuptools.command.install import install
 
 from chaser import __version__
 
@@ -7,6 +10,27 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+class Install(install):
+    def run(self):
+        install.run(self)
+        # Install locales
+        if self.root:
+            usr_dir = "%s/usr" % self.root
+        else:
+            usr_dir = "/usr"
+        locale_dir = os.path.join(usr_dir, "share/locale")
+
+        print("Installing locales...")
+        for filename in glob.glob1("po", "*.po"):
+            lang = filename.rsplit(".", 1)[0]
+            os.system("msgfmt po/%s.po -o po/%s.mo" % (lang, lang))
+            try:
+                os.makedirs(os.path.join(locale_dir, "%s/LC_MESSAGES" % lang))
+            except OSError:
+                pass
+            shutil.copy("po/%s.mo" % lang, os.path.join(locale_dir, "%s/LC_MESSAGES" % lang, "%s.mo" % "chaser"))
+
 
 setup(
     name='chaser',
@@ -36,4 +60,7 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Topic :: System :: Software Distribution',
     ],
+    cmdclass={
+        'install': Install,
+    },
 )
